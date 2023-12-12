6810563410d85047e0086ed0eb8ba586104108ee
Ahmed Nadar <ahmedfay@hotmail.com>
2023-12-11 23:38:06 -0500

    Add new files and update routes and layout as a placeholder till next update

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,12 +5,19 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,html,slim,haml}',
+    './app/components/**/*', // Rails view components
+    './app/assets/images/**/*',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {"50":"#fff1f2","100":"#ffe4e6","200":"#fecdd3","300":"#fda4af","400":"#fb7185","500":"#f43f5e","600":"#e11d48","700":"#be123c","800":"#9f1239","900":"#881337","950":"#4c0519"}
+        ,
+        secondary: {"50":"#ecfeff","100":"#cffafe","200":"#a5f3fc","300":"#67e8f9","400":"#22d3ee","500":"#06b6d4","600":"#0891b2","700":"#0e7490","800":"#155e75","900":"#164e63","950":"#083344"}
       },
     },
   },
