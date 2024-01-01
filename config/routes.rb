083250dab26fe96e40f444028a555d432d6906a3
Ahmed Nadar <ahmedfay@hotmail.com>
2024-01-01 17:09:31 -0500

    Add pages, tailwind config and a hero image.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'pages/home', as: 'home'
-  get 'pages/about', as: 'about'
+  get 'pages/about', to: 'pages#about', as: 'about'
   get 'pages/contact', as: 'contact'
+  get 'pages/toronto', as: 'toronto'
+  get 'pages/framework', as: 'framework'
+  get 'pages/people', as: 'people'
+  get 'pages/conf', as: 'conf'
 end
 
 
